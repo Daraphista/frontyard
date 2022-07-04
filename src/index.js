b085af1b2bd6c8daf5a2b4c863b879b535016d31
daraphista <angeloraphaelmendoza@gmail.com>
2022-07-04 13:03:09 +0800

    feat(nav): add logic to decide animation and positioning

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./style.css";
+
+// logic to decide the navbar's animation and positioning
+
+const main = document.querySelector("main");
+
+const userScrolledToMain = () => {
+  const mainPosition = main.offsetTop;
+  const topOfViewPort = window.scrollY;
+  return topOfViewPort > mainPosition;
+};
+
+const navbar = document.querySelector("nav");
+
+navbar.classList.remove("fixed");
+
+window.addEventListener("scroll", () => {
+  console.log(userScrolledToMain());
+  if (userScrolledToMain()) {
+    navbar.classList.remove("absolute");
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+    navbar.classList.add("absolute");
+  }
+});
