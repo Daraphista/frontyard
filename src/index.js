a75bcc380928a75fe60243730626a65321932119
daraphista <angeloraphaelmendoza@gmail.com>
2022-07-05 22:49:30 +0800

    feat(image carousel): add scroll buttons

--- src/index.js
+++ src/index.js
@@ -24,3 +24,47 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("absolute");
   }
 });
+
+// image carousel functionality
+
+const carousel = document.querySelector("#carousel");
+const carouselButtonLeft = document.querySelector("#carousel-button-left");
+const carouselButtonRight = document.querySelector("#carousel-button-right");
+
+const scrolledToStart = () => {
+  return carousel.scrollLeft === 0;
+};
+
+const scrolledToEnd = () => {
+  return carousel.scrollLeft + carousel.clientWidth === carousel.scrollWidth;
+};
+
+carousel.addEventListener("scroll", () => {
+  // logic to decide if carousel left button should show or not
+  if (scrolledToStart()) {
+    carouselButtonLeft.classList.remove("group-hover:flex");
+    carouselButtonLeft.classList.add("group-hover:hidden");
+  } else if (!scrolledToStart()) {
+    carouselButtonLeft.classList.remove("group-hover:hidden");
+    carouselButtonLeft.classList.add("group-hover:flex");
+  }
+
+  // logic to decide if carousel right button should show or not
+  if (scrolledToEnd()) {
+    carouselButtonRight.classList.remove("group-hover:flex");
+    carouselButtonRight.classList.add("group-hover:hidden");
+  } else if (!scrolledToEnd()) {
+    carouselButtonRight.classList.remove("group-hover:hidden");
+    carouselButtonRight.classList.add("group-hover:flex");
+  }
+});
+
+carouselButtonLeft.addEventListener("click", () => {
+  // scroll to the left when clicked
+  carousel.scrollBy({ left: -500, behavior: "smooth" });
+});
+
+carouselButtonRight.addEventListener("click", () => {
+  // scroll to the right when clicked
+  carousel.scrollBy({ left: 500, behavior: "smooth" });
+});
